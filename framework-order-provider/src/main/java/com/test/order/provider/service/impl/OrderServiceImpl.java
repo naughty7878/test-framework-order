cbd7fc5438f3623e67eb137944dd67d988e11a97
god <naughty7878@163.com>
2020-07-04 22:42:10 +0800

    微服务调用 增加第二种方式

--- framework-order-provider/src/main/java/com/test/order/provider/service/impl/OrderServiceImpl.java
+++ framework-order-provider/src/main/java/com/test/order/provider/service/impl/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.test.order.provider.service.impl;
 import com.test.common.entities.CommonResult;
 import com.test.order.provider.service.OrderService;
 import com.test.payment.api.bo.PaymentBo;
+import com.test.payment.client.PaymentClient;
 import com.test.payment.client.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +17,14 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public CommonResult get(Long id) {
+
         PaymentBo paymentBo = new PaymentBo();
         paymentBo.setId(id);
-        return paymentService.get(paymentBo);
+
+        // 方法一，使用Feign直接调用
+//        return paymentService.get(paymentBo);
+        // 方法二，使用业务提供的Client调用
+        return PaymentClient.get(paymentBo);
+
     }
 }
